3db1b93c067186eb4bc54b2325f685908603dada
mayayan1121 <70425010+mayayan1121@users.noreply.github.com>
2022-12-20 21:34:11 +0800

    Update cracker.js

--- cracker.js
+++ cracker.js
@@ -7,13 +7,14 @@ const joke = document.getElementById("joke");
 const punchline = document.getElementById("punchline");
 
 let counter = 0
-let jokes =[{Q: "Where would you find a snowman dancing?" ,A: "At a snowball!"},{Q: "What do you call a blind reindeer?" ,A: "No-eye deer."}, {Q: "What do you call an old snowman?" ,A: "Water!"}, {Q: "What do snowmen have for breakfast?" ,A: "Snowflakes!"}, {Q: "What did the English teacher call Santa's helpers?" ,A: "Subordinate Clauses!"}, {Q:"What’s every parent’s favorite Christmas Carol?" ,A: "Silent Night~"}]
+let jokes =[{Q: "What did the English teacher call Santa's helpers?" ,A: "Subordinate Clauses!"}, {Q: "Where would you find a snowman dancing?" ,A: "At a snowball!"},{Q: "What do you call a blind reindeer?" ,A: "No-eye deer."}, {Q: "What do you call an old snowman?" ,A: "Water!"},{Q: "What did the English teacher call Santa's helpers?" ,A: "Subordinate Clauses!"}, {Q: "What do snowmen have for breakfast?" ,A: "Snowflakes!"}, {Q:"What’s every parent’s favorite Christmas Carol?" ,A: "Silent Night~"}]
 let num = Math.floor(Math.random() * jokes.length)
 
 cracker.addEventListener('click', () => {
   if(counter < 0){
     counter++
-  } else{
+  } 
+  else{
     wishes.textContent = "I hope the holidays bring a chance for you to recharge! Enjoy some xmas joke here~"
     maya.textContent = "-Maya"
     instructions.textContent = ""
@@ -26,3 +27,5 @@ cracker.addEventListener('click', () => {
     cracker.style.transform = "scaleX(1)"
   }
 })
+
+
